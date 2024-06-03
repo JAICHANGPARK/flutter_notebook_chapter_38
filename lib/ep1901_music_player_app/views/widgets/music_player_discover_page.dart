d465b052368161703dafd285d0baa0b23e6d8303
JAICHANGPARK <aristojeff@gmail.com>
2024-06-03 17:40:31 +0900

    Update music_player_discover_page.dart

---
+++ lib/ep1901_music_player_app/views/widgets/music_player_discover_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+
+class MusicPlayerDiscoverPage extends StatefulWidget {
+  const MusicPlayerDiscoverPage({super.key});
+
+  @override
+  State<MusicPlayerDiscoverPage> createState() => _MusicPlayerDiscoverPageState();
+}
+
+class _MusicPlayerDiscoverPageState extends State<MusicPlayerDiscoverPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+
+      ],
+    );
+  }
+}
