787cf18525a9100b4dd9d1268e894d4b15f948f5
JAICHANGPARK <aristojeff@gmail.com>
2024-06-03 17:37:22 +0900

    Update ep1901_main.dart

---
+++ lib/ep1901_music_player_app/ep1901_main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+
+class MusicPlayerApp extends StatelessWidget {
+  const MusicPlayerApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      
+    );
+  }
+}
