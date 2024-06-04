465c3421331ac096108d56d6fc2241ef7a40fcd4
JAICHANGPARK <aristojeff@gmail.com>
2024-06-04 18:55:18 +0900

    Update music_player_discover_page.dart

--- lib/ep1901_music_player_app/views/widgets/music_player_discover_page.dart
+++ lib/ep1901_music_player_app/views/widgets/music_player_discover_page.dart
@@ -12,82 +12,83 @@ class _MusicPlayerDiscoverPageState extends State<MusicPlayerDiscoverPage> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(
-              left: 16,
-              top: 16,
-              bottom: 16,
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  "Discover",
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 24,
-                    color: Colors.white,
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(
+                left: 16,
+                top: 16,
+                bottom: 16,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Discover",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 24,
+                      color: Colors.white,
+                    ),
                   ),
-                ),
-                Gap(12),
-                Container(
-                  height: 200,
-                  color: Colors.blue,
-                ),
-              ],
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  "Popular Artist",
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 24,
-                    color: Colors.white,
+                  Gap(12),
+                  Container(
+                    height: 200,
+                    color: Colors.blue,
                   ),
-                ),
-                Gap(8),
-                Container(
-                  height: 200,
-                  color: Colors.orange,
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  "Popular Artist",
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 24,
-                    color: Colors.white,
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Popular Artist",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 24,
+                      color: Colors.white,
+                    ),
                   ),
-                ),
-                Gap(8),
-                Column(
-                  children: List.generate(10, (e){
-                    return Container(
-                      height: 64,
-                      color: Colors.pink,
-                    );
-                  }),
-                )
-              ],
+                  Gap(8),
+                  Container(
+                    height: 200,
+                    color: Colors.orange,
+                  ),
+                ],
+              ),
             ),
-          )
-
-        ],
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Popular Artist",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 24,
+                      color: Colors.white,
+                    ),
+                  ),
+                  Gap(8),
+                  Column(
+                    children: List.generate(10, (e) {
+                      return Container(
+                        height: 64,
+                        color: Colors.pink,
+                      );
+                    }),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
