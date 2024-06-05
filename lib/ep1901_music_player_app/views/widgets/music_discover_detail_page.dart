f0dfed930aa5bab7b7388271097ce03abbc75308
JAICHANGPARK <aristojeff@gmail.com>
2024-06-05 20:13:41 +0900

    Update music_discover_detail_page.dart

--- lib/ep1901_music_player_app/views/widgets/music_discover_detail_page.dart
+++ lib/ep1901_music_player_app/views/widgets/music_discover_detail_page.dart
@@ -1,2 +1,17 @@
 import 'package:flutter/material.dart';
 
+class MusicDiscoverDetailPage extends StatefulWidget {
+  const MusicDiscoverDetailPage({super.key});
+
+  @override
+  State<MusicDiscoverDetailPage> createState() => _MusicDiscoverDetailPageState();
+}
+
+class _MusicDiscoverDetailPageState extends State<MusicDiscoverDetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [],
+    );
+  }
+}
