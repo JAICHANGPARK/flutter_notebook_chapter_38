76c1e170118fd2a95cfcfab3876ad0f93bc40ee9
JAICHANGPARK <aristojeff@gmail.com>
2024-06-05 20:22:14 +0900

    Update music_discover_detail_page.dart

--- lib/ep1901_music_player_app/views/widgets/music_discover_detail_page.dart
+++ lib/ep1901_music_player_app/views/widgets/music_discover_detail_page.dart
@@ -10,89 +10,95 @@ class MusicDiscoverDetailPage extends StatefulWidget {
 class _MusicDiscoverDetailPageState extends State<MusicDiscoverDetailPage> {
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Positioned.fill(
-          bottom: 72,
-          child: Column(
-            children: [
-              Expanded(
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Colors.black,
-                    image: DecorationImage(
-                      image: NetworkImage(
-                        "https://cdn.pixabay.com/photo/2015/08/05/10/31/an-pierle-876094_1280.jpg",
+    return Material(
+      child: Stack(
+        children: [
+          Positioned.fill(
+            bottom: 72,
+            child: Column(
+              children: [
+                Expanded(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.black,
+                      image: DecorationImage(
+                        image: NetworkImage(
+                          "https://cdn.pixabay.com/photo/2015/08/05/10/31/an-pierle-876094_1280.jpg",
+                        ),
+                        fit: BoxFit.cover,
                       ),
-                      fit: BoxFit.cover,
                     ),
                   ),
                 ),
-              ),
-              Expanded(
-                child: Container(color: Colors.black),
-              ),
-            ],
-          ),
-        ),
-        Positioned.fill(
-          left: 16,
-          right: 16,
-          top: 24,
-          child: SafeArea(
-            child: Column(
-              children: [
-                Row(
-                  children: [
-                    Container(
-                      height: 48,
-                      width: 48,
-                      decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        border: Border.all(
-                          color: Colors.blueGrey,
-                          width: 1.5,
-                        ),
-                        color: Colors.grey,
-                      ),
-                      child: IconButton(
-                        icon: Icon(
-                          Icons.arrow_back,
-                        ),
-                        color: Colors.white,
-                        onPressed: () {
-                          Navigator.of(context).pop();
-                        },
-                      ),
-                    ),
-                    Container(
-                      height: 48,
-                      width: 48,
-                      decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        border: Border.all(
-                          color: Colors.blueGrey,
-                          width: 1.5,
-                        ),
-                        color: Colors.grey,
-                      ),
-                      child: IconButton(
-                        icon: Icon(
-                          Icons.more_horiz,
-                        ),
-                        color: Colors.white,
-                        onPressed: () {
-                          Navigator.of(context).pop();
-                        },
-                      ),
-                    )
-                  ],
-                )
+                Expanded(
+                  child: Container(color: Colors.black),
+                ),
               ],
             ),
           ),
-        )
-      ],
+          Positioned.fill(
+            left: 16,
+            right: 16,
+            top: 24,
+            child: SafeArea(
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                        height: 48,
+                        width: 48,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          border: Border.all(
+                            color: Colors.blueGrey,
+                            width: 1.5,
+                          ),
+                          color: Colors.grey,
+                        ),
+                        child: IconButton(
+                          icon: Icon(
+                            Icons.arrow_back,
+                          ),
+                          color: Colors.white,
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                        ),
+                      ),
+                      Container(
+                        height: 48,
+                        width: 48,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          border: Border.all(
+                            color: Colors.blueGrey,
+                            width: 1.5,
+                          ),
+                          color: Colors.grey,
+                        ),
+                        child: IconButton(
+                          icon: Icon(
+                            Icons.more_horiz,
+                          ),
+                          color: Colors.white,
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                  Text(
+                    "The Amazing One Band",
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
