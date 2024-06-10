d313bae18ef15df9aba728de5d208ec8e56211e6
JAICHANGPARK <aristojeff@gmail.com>
2024-06-10 20:03:44 +0900

    Update video_main_page.dart

--- lib/ep1913_video_app/views/video_main_page.dart
+++ lib/ep1913_video_app/views/video_main_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class VideoMainPage extends StatefulWidget {
   const VideoMainPage({super.key});
 
@@ -11,6 +10,25 @@ class VideoMainPage extends StatefulWidget {
 class _VideoMainPageState extends State<VideoMainPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        surfaceTintColor: Colors.white,
+        foregroundColor: Colors.blueGrey,
+        leading: IconButton(
+          onPressed: () {},
+          icon: Icon(Icons.account_circle_outlined),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.search,
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
