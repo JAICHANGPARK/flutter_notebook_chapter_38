c9d3d94f7dd5701192c8fe8d64765eb6cd3ad475
JAICHANGPARK <aristojeff@gmail.com>
2024-06-10 17:51:47 +0900

    Update beauty_home_page.dart

--- lib/ep1908_beauty_app/views/beauty_home_page.dart
+++ lib/ep1908_beauty_app/views/beauty_home_page.dart
@@ -15,37 +15,44 @@ class BeautyHomePage extends StatelessWidget {
                 Expanded(
                   child: ListView.builder(
                     itemBuilder: (context, index) {
-                      return Card(
-                        child: Padding(
-                          padding: const EdgeInsets.all(16.0),
-                          child: Column(
-                            children: [
-                              Container(
-                                height: 62,
-                                color: Colors.blueGrey,
-                              ),
-                              Text("Creams"),
-                              Text("Day Cream"),
-                              Container(
-                                decoration: BoxDecoration(
-                                  color: Colors.white,
+                      return Padding(
+                        padding: const EdgeInsets.only(bottom: 12),
+                        child: Card(
+                          child: Padding(
+                            padding: const EdgeInsets.all(16.0),
+                            child: Column(
+                              children: [
+                                Container(
+                                  height: 62,
+                                  color: Colors.blueGrey,
                                 ),
-                                child: Row(
-                                  children: [
-                                    Gap(8),
-                                    Text("\$79.00"),
-                                    Spacer(),
-                                    CircleAvatar(
-                                      radius: 18,
-                                      backgroundColor: Colors.black,
-                                      child: Icon(
-                                        Icons.add_shopping_cart,
+                                Text("Creams"),
+                                Text("Day Cream"),
+                                Container(
+                                  decoration:
+                                      BoxDecoration(color: Colors.white, borderRadius: BorderRadius.circular(16)),
+                                  child: Row(
+                                    children: [
+                                      Gap(8),
+                                      Text("\$79.00"),
+                                      Spacer(),
+                                      Padding(
+                                        padding: const EdgeInsets.all(2.0),
+                                        child: CircleAvatar(
+                                          radius: 16,
+                                          backgroundColor: Colors.black,
+                                          foregroundColor: Colors.white,
+                                          child: Icon(
+                                            Icons.add_shopping_cart,
+                                            size: 16,
+                                          ),
+                                        ),
                                       ),
-                                    ),
-                                  ],
-                                ),
-                              )
-                            ],
+                                    ],
+                                  ),
+                                )
+                              ],
+                            ),
                           ),
                         ),
                       );
