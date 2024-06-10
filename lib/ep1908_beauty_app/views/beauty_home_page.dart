adf21e6b8c7e3aa2839f4ddbc70124bb778b70a1
JAICHANGPARK <aristojeff@gmail.com>
2024-06-10 17:52:24 +0900

    Update beauty_home_page.dart

--- lib/ep1908_beauty_app/views/beauty_home_page.dart
+++ lib/ep1908_beauty_app/views/beauty_home_page.dart
@@ -10,60 +10,108 @@ class BeautyHomePage extends StatelessWidget {
       body: Stack(
         children: [
           Positioned.fill(
-            child: Row(
-              children: [
-                Expanded(
-                  child: ListView.builder(
-                    itemBuilder: (context, index) {
-                      return Padding(
-                        padding: const EdgeInsets.only(bottom: 12),
-                        child: Card(
-                          child: Padding(
-                            padding: const EdgeInsets.all(16.0),
-                            child: Column(
-                              children: [
-                                Container(
-                                  height: 62,
-                                  color: Colors.blueGrey,
-                                ),
-                                Text("Creams"),
-                                Text("Day Cream"),
-                                Container(
-                                  decoration:
-                                      BoxDecoration(color: Colors.white, borderRadius: BorderRadius.circular(16)),
-                                  child: Row(
-                                    children: [
-                                      Gap(8),
-                                      Text("\$79.00"),
-                                      Spacer(),
-                                      Padding(
-                                        padding: const EdgeInsets.all(2.0),
-                                        child: CircleAvatar(
-                                          radius: 16,
-                                          backgroundColor: Colors.black,
-                                          foregroundColor: Colors.white,
-                                          child: Icon(
-                                            Icons.add_shopping_cart,
-                                            size: 16,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 24.0,
+              ),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: ListView.builder(
+                      itemBuilder: (context, index) {
+                        return Padding(
+                          padding: const EdgeInsets.only(bottom: 12),
+                          child: Card(
+                            child: Padding(
+                              padding: const EdgeInsets.all(16.0),
+                              child: Column(
+                                children: [
+                                  Container(
+                                    height: 62,
+                                    color: Colors.blueGrey,
+                                  ),
+                                  Text("Creams"),
+                                  Text("Day Cream"),
+                                  Container(
+                                    decoration:
+                                        BoxDecoration(color: Colors.white, borderRadius: BorderRadius.circular(16)),
+                                    child: Row(
+                                      children: [
+                                        Gap(8),
+                                        Text("\$79.00"),
+                                        Spacer(),
+                                        Padding(
+                                          padding: const EdgeInsets.all(2.0),
+                                          child: CircleAvatar(
+                                            radius: 16,
+                                            backgroundColor: Colors.black,
+                                            foregroundColor: Colors.white,
+                                            child: Icon(
+                                              Icons.add_shopping_cart,
+                                              size: 16,
+                                            ),
                                           ),
                                         ),
-                                      ),
-                                    ],
-                                  ),
-                                )
-                              ],
+                                      ],
+                                    ),
+                                  )
+                                ],
+                              ),
                             ),
                           ),
-                        ),
-                      );
-                    },
+                        );
+                      },
+                    ),
                   ),
-                ),
-                Expanded(
+                  Expanded(
                     child: ListView.builder(
-                  itemBuilder: (context, index) {},
-                ))
-              ],
+                      itemBuilder: (context, index) {
+                        return Padding(
+                          padding: const EdgeInsets.only(bottom: 12),
+                          child: Card(
+                            child: Padding(
+                              padding: const EdgeInsets.all(16.0),
+                              child: Column(
+                                children: [
+                                  Container(
+                                    height: 62,
+                                    color: Colors.blueGrey,
+                                  ),
+                                  Text("Creams"),
+                                  Text("Day Cream"),
+                                  Container(
+                                    decoration:
+                                        BoxDecoration(color: Colors.white, borderRadius: BorderRadius.circular(16)),
+                                    child: Row(
+                                      children: [
+                                        Gap(8),
+                                        Text("\$79.00"),
+                                        Spacer(),
+                                        Padding(
+                                          padding: const EdgeInsets.all(2.0),
+                                          child: CircleAvatar(
+                                            radius: 16,
+                                            backgroundColor: Colors.black,
+                                            foregroundColor: Colors.white,
+                                            child: Icon(
+                                              Icons.add_shopping_cart,
+                                              size: 16,
+                                            ),
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  )
+                                ],
+                              ),
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                ],
+              ),
             ),
           )
         ],
