f3e5e5bf502771e70ce028069095845b7393fd91
JAICHANGPARK <aristojeff@gmail.com>
2024-06-10 17:46:03 +0900

    Update beauty_home_page.dart

--- lib/ep1908_beauty_app/views/beauty_home_page.dart
+++ lib/ep1908_beauty_app/views/beauty_home_page.dart
@@ -6,6 +6,12 @@ class BeautyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Stack(
+        children: [
+
+        ],
+      ),
+    );
   }
 }
