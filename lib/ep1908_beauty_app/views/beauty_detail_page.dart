722df459e5b72f5f14b9c14823cdf9791f8de2a5
JAICHANGPARK <aristojeff@gmail.com>
2024-06-10 18:51:20 +0900

    Create beauty_detail_page.dart

---
+++ lib/ep1908_beauty_app/views/beauty_detail_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class BeautyDetailPage extends StatefulWidget {
+  const BeautyDetailPage({super.key});
+
+  @override
+  State<BeautyDetailPage> createState() => _BeautyDetailPageState();
+}
+
+class _BeautyDetailPageState extends State<BeautyDetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
