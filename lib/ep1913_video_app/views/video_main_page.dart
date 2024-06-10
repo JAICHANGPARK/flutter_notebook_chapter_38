5810fa32dd2833c785e0dfc1acc4c88144d8ea21
JAICHANGPARK <aristojeff@gmail.com>
2024-06-10 19:59:32 +0900

    Create video_main_page.dart

---
+++ lib/ep1913_video_app/views/video_main_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class VideoMainPage extends StatefulWidget {
+  const VideoMainPage({super.key});
+
+  @override
+  State<VideoMainPage> createState() => _VideoMainPageState();
+}
+
+class _VideoMainPageState extends State<VideoMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
