9a1d4673807a9943623bc8e91788049ea43e4eed
JAICHANGPARK <aristojeff@gmail.com>
2024-06-10 17:50:24 +0900

    Update beauty_home_page.dart

--- lib/ep1908_beauty_app/views/beauty_home_page.dart
+++ lib/ep1908_beauty_app/views/beauty_home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 
 class BeautyHomePage extends StatelessWidget {
   const BeautyHomePage({super.key});
@@ -15,20 +16,33 @@ class BeautyHomePage extends StatelessWidget {
                   child: ListView.builder(
                     itemBuilder: (context, index) {
                       return Card(
-                        child: Column(
-                          children: [
-                            Container(
-                              height: 62,
-                              color: Colors.blueGrey,
-                            ),
-                            Text("Creams"),
-                            Text("Day Cream"),
-                            Container(
-                              decoration: BoxDecoration(
-                                color: Colors.white,
+                        child: Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: Column(
+                            children: [
+                              Container(
+                                height: 62,
+                                color: Colors.blueGrey,
                               ),
-                            )
-                          ],
+                              Text("Creams"),
+                              Text("Day Cream"),
+                              Container(
+                                decoration: BoxDecoration(
+                                  color: Colors.white,
+                                ),
+                                child: Row(
+                                  children: [
+                                    Gap(8),
+                                    Text("\$79.00"),
+                                    Spacer(),
+                                    CircleAvatar(
+                                      backgroundColor: Colors.black,
+                                    ),
+                                  ],
+                                ),
+                              )
+                            ],
+                          ),
                         ),
                       );
                     },
