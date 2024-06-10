d14b4d0e648668d6fb4ef85daed9fed9916ea1ef
JAICHANGPARK <aristojeff@gmail.com>
2024-06-10 18:58:15 +0900

    Update beauty_detail_page.dart

--- lib/ep1908_beauty_app/views/beauty_detail_page.dart
+++ lib/ep1908_beauty_app/views/beauty_detail_page.dart
@@ -11,16 +11,31 @@ class _BeautyDetailPageState extends State<BeautyDetailPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          Positioned.fill(
-            child: Image.network(
+      body: Container(
+        height: MediaQuery.sizeOf(context).height,
+        width: MediaQuery.sizeOf(context).width,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: NetworkImage(
               "https://cdn.pixabay.com/photo/2023/11/10/02/30/woman-8378634_1280.jpg",
-              fit: BoxFit.cover,
             ),
-          )
-        ],
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Column(
+          children: [],
+        ),
       ),
+      // body: Stack(
+      //   children: [
+      //     Positioned.fill(
+      //       child: Image.network(
+      //         "https://cdn.pixabay.com/photo/2023/11/10/02/30/woman-8378634_1280.jpg",
+      //         fit: BoxFit.cover,
+      //       ),
+      //     ),
+      //   ],
+      // ),
     );
   }
 }
