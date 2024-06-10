626b35e76f0d8c6e852957f61a4b67f19104aa34
JAICHANGPARK <aristojeff@gmail.com>
2024-06-10 17:44:53 +0900

    Update ep1908_main.dart

---
+++ lib/ep1908_beauty_app/ep1908_main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+
+}
+
+class BeautyApp extends StatelessWidget {
+  const BeautyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+    );
+  }
+}
