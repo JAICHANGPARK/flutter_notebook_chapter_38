1e5aca258ef89d1b9681cb7b065d7bcc4817aa21
JAICHANGPARK <aristojeff@gmail.com>
2024-06-10 18:52:31 +0900

    Update beauty_detail_page.dart

--- lib/ep1908_beauty_app/views/beauty_detail_page.dart
+++ lib/ep1908_beauty_app/views/beauty_detail_page.dart
@@ -10,6 +10,17 @@ class BeautyDetailPage extends StatefulWidget {
 class _BeautyDetailPageState extends State<BeautyDetailPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned.fill(
+            child: Image.network(
+              "https://cdn.pixabay.com/photo/2023/11/10/02/30/woman-8378634_1280.jpg",
+              fit: BoxFit.cover,
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
