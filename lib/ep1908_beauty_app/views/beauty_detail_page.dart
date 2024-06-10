acc0c50145900fd8d0d3a0a474de4e15f8a37c9c
JAICHANGPARK <aristojeff@gmail.com>
2024-06-10 19:44:41 +0900

    Update beauty_detail_page.dart

--- lib/ep1908_beauty_app/views/beauty_detail_page.dart
+++ lib/ep1908_beauty_app/views/beauty_detail_page.dart
@@ -1,6 +1,7 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 
 class BeautyDetailPage extends StatefulWidget {
   const BeautyDetailPage({super.key});
@@ -151,8 +152,8 @@ class _BeautyDetailPageState extends State<BeautyDetailPage> {
                       ),
                     ),
                     Positioned.fill(
-                      top: 24,
-                      bottom: 24,
+                      top: 16,
+                      bottom: 16,
                       child: Column(
                         children: [
                           Text(
@@ -162,20 +163,99 @@ class _BeautyDetailPageState extends State<BeautyDetailPage> {
                               fontSize: 21,
                             ),
                           ),
+                          Gap(12),
                           Expanded(
                             child: Container(
+                              margin: EdgeInsets.only(left: 16),
                               decoration: BoxDecoration(
                                 color: Colors.blue,
                               ),
+                              child: ListView.builder(
+                                itemBuilder: (context, index) {
+                                  return Padding(
+                                    padding: const EdgeInsets.only(bottom: 12),
+                                    child: SizedBox(
+                                      child: InkWell(
+                                        onTap: () {
+                                          Navigator.of(context).push(
+                                            MaterialPageRoute(
+                                              builder: (context) => BeautyDetailPage(),
+                                            ),
+                                          );
+                                        },
+                                        child: Card(
+                                          child: Padding(
+                                            padding: const EdgeInsets.all(16.0),
+                                            child: Column(
+                                              children: [
+                                                Container(
+                                                  height: 74,
+                                                  width: 62,
+                                                  color: Colors.blueGrey,
+                                                ),
+                                                const Gap(12),
+                                                const Text("Creams"),
+                                                Gap(4),
+                                                const Text(
+                                                  "Day Cream",
+                                                  style: TextStyle(
+                                                    fontSize: 12,
+                                                  ),
+                                                ),
+                                                Gap(8),
+                                                Container(
+                                                  decoration: BoxDecoration(color: Colors.white, borderRadius: BorderRadius.circular(16)),
+                                                  child: const Row(
+                                                    children: [
+                                                      Gap(8),
+                                                      Text(
+                                                        "\$79.00",
+                                                        style: TextStyle(
+                                                          fontWeight: FontWeight.bold,
+                                                        ),
+                                                      ),
+                                                      Spacer(),
+                                                      Padding(
+                                                        padding: EdgeInsets.all(2.0),
+                                                        child: CircleAvatar(
+                                                          radius: 16,
+                                                          backgroundColor: Colors.black,
+                                                          foregroundColor: Colors.white,
+                                                          child: Icon(
+                                                            Icons.add_shopping_cart,
+                                                            size: 16,
+                                                          ),
+                                                        ),
+                                                      ),
+                                                    ],
+                                                  ),
+                                                )
+                                              ],
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  );
+                                },
+                              ),
                             ),
                           ),
+                          Gap(12),
                           Container(
+                            padding: EdgeInsets.symmetric(vertical: 12),
+                            margin: EdgeInsets.symmetric(horizontal: 16),
                             decoration: BoxDecoration(
                               color: Colors.black,
+                              borderRadius: BorderRadius.circular(24),
                             ),
                             child: Center(
                               child: Text(
                                 "View all products",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 18,
+                                ),
                               ),
                             ),
                           )
