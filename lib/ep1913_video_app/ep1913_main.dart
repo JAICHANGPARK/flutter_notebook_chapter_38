0a886e3136df3b24e9949e8b3c3e8e690a3fe78a
JAICHANGPARK <aristojeff@gmail.com>
2024-06-10 19:57:43 +0900

    Create ep1913_main.dart

---
+++ lib/ep1913_video_app/ep1913_main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  
+}
+
+class VideoMobileApp extends StatelessWidget {
+  const VideoMobileApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
