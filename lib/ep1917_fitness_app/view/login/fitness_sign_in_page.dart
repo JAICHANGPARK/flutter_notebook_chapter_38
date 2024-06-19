2268e7566695cdea8d6a825f410dcd7e9d09400f
JAICHANGPARK <aristojeff@gmail.com>
2024-06-19 19:34:20 +0900

    Update fitness_sign_in_page.dart

--- lib/ep1917_fitness_app/view/login/fitness_sign_in_page.dart
+++ lib/ep1917_fitness_app/view/login/fitness_sign_in_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class FitnessSignInPage extends StatefulWidget {
   const FitnessSignInPage({super.key});
 
@@ -11,6 +10,67 @@ class FitnessSignInPage extends StatefulWidget {
 class _FitnessSignInPageState extends State<FitnessSignInPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned.fill(
+            child: Image.network(
+              "https://cdn.pixabay.com/photo/2018/02/12/23/17/people-3149372_1280.jpg",
+              fit: BoxFit.cover,
+            ),
+          ),
+          Positioned(
+            left: 24,
+            top: 120,
+            right: 24,
+            bottom: 32,
+            child: Column(
+              children: [
+                Text("Whenever you are\health is number one"),
+                Text("There is no instant way to a healthy life"),
+                Container(
+                  height: 62,
+                  decoration: BoxDecoration(
+                    color: Colors.greenAccent,
+                  ),
+                  child: Row(
+                    children: [
+                      Icon(Icons.mail_outline),
+                      Text(
+                        "Sign In with Email",
+                      )
+                    ],
+                  ),
+                ),
+                Container(
+                  height: 62,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                  ),
+                  child: Row(
+                    children: [
+                      Text(
+                        "Sign In with Google",
+                      )
+                    ],
+                  ),
+                ),
+                Row(
+                  children: [
+                    Text("Not a member?"),
+                    TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        "Sign Up",
+                      ),
+                    )
+                  ],
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
