5b78a4079ed0fc4f9fd7b471c286da204be40a84
JAICHANGPARK <aristojeff@gmail.com>
2024-06-19 19:28:35 +0900

    Update fitness_sign_up_page.dart

---
+++ lib/ep1917_fitness_app/view/login/fitness_sign_up_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FitnessSignUpPage extends StatefulWidget {
+  const FitnessSignUpPage({super.key});
+
+  @override
+  State<FitnessSignUpPage> createState() => _FitnessSignUpPageState();
+}
+
+class _FitnessSignUpPageState extends State<FitnessSignUpPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
