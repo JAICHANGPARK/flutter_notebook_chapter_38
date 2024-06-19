7cc394d6ceda1b65649fb31e0ea5b4cc89544416
JAICHANGPARK <aristojeff@gmail.com>
2024-06-19 19:27:53 +0900

    Update ep1917_main.dart

--- lib/ep1917_fitness_app/ep1917_main.dart
+++ lib/ep1917_fitness_app/ep1917_main.dart
@@ -5,16 +5,22 @@ void main() {
   runApp(FitnessApp());
 }
 
-
 class FitnessApp extends StatelessWidget {
-   FitnessApp({super.key});
+  FitnessApp({super.key});
 
-  final router = GoRouter(routes: [],);
+  final router = GoRouter(
+    routes: [
+      GoRoute(
+        path: "/sign-in",
+        builder: (context, state) {},
+      )
+    ],
+  );
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
-          routerConfig: router,
+      routerConfig: router,
     );
   }
 }
