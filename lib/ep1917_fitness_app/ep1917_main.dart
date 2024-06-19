4d287099fde35588a201a18469e1bff8f2d4f4ee
JAICHANGPARK <aristojeff@gmail.com>
2024-06-19 19:28:37 +0900

    Update ep1917_main.dart

--- lib/ep1917_fitness_app/ep1917_main.dart
+++ lib/ep1917_fitness_app/ep1917_main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_chapter_38/ep1917_fitness_app/view/login/fitness_sign_in_page.dart';
 import 'package:go_router/go_router.dart';
 
 void main() {
@@ -9,10 +10,13 @@ class FitnessApp extends StatelessWidget {
   FitnessApp({super.key});
 
   final router = GoRouter(
+    initialLocation: "/sign-in",
     routes: [
       GoRoute(
         path: "/sign-in",
-        builder: (context, state) {},
+        builder: (context, state) {
+          return FitnessSignInPage();
+        },
       )
     ],
   );
@@ -20,6 +24,7 @@ class FitnessApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
+
       routerConfig: router,
     );
   }
