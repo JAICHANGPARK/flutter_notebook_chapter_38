d9e27891a749ec6ba925c8f83f4e7d10989d1e1b
JAICHANGPARK <aristojeff@gmail.com>
2024-06-24 20:01:32 +0900

    Update electronic_home_page.dart

--- lib/ep1922_electronic_app/view/electronic_home_page.dart
+++ lib/ep1922_electronic_app/view/electronic_home_page.dart
@@ -11,41 +11,74 @@ class _ElectronicHomePageState extends State<ElectronicHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: BottomNavigationBar(
-        backgroundColor: Color(0xff101014),
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.home_filled,
-            ),
-            label: "Home",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.emoji_objects_outlined,
-            ),
-            label: "Feeds",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.favorite_outline,
-            ),
-            label: "Likes",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.shopping_bag_outlined,
-            ),
-            label: "Carts",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.person_2_outlined,
-            ),
-            label: "Profiles",
-          ),
-        ],
+      body: SafeArea(
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white.withOpacity(.1),
+                    ),
+                    child: Row(
+                      children: [
+                        CircleAvatar(),
+                        Expanded(child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
 
+                          ],
+                        ))
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+      bottomNavigationBar: SizedBox(
+        height: 72,
+        child: BottomNavigationBar(
+          backgroundColor: Color.fromRGBO(29, 30, 34, 1),
+          selectedItemColor: Colors.white,
+          unselectedItemColor: Colors.grey,
+          type: BottomNavigationBarType.fixed,
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.home_filled,
+              ),
+              label: "Home",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.emoji_objects_outlined,
+              ),
+              label: "Feeds",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.favorite_outline,
+              ),
+              label: "Likes",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.shopping_bag_outlined,
+              ),
+              label: "Carts",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.person_2_outlined,
+              ),
+              label: "Profiles",
+            ),
+          ],
+        ),
       ),
     );
   }
