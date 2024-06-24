9a62c044d0748497fe9ef3252bc84a38c9bd9025
JAICHANGPARK <aristojeff@gmail.com>
2024-06-24 19:52:12 +0900

    Update ep1922_main.dart

---
+++ lib/ep1922_electronic_app/ep1922_main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+
+void main() {
+
+}
+
+class ElectronicMobileApp extends StatelessWidget {
+  const ElectronicMobileApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
