b35026525693060ef1acdfc702ec12ff61e7c027
JAICHANGPARK <aristojeff@gmail.com>
2024-06-24 19:59:10 +0900

    Update electronic_home_page.dart

--- lib/ep1922_electronic_app/view/electronic_home_page.dart
+++ lib/ep1922_electronic_app/view/electronic_home_page.dart
@@ -12,19 +12,40 @@ class _ElectronicHomePageState extends State<ElectronicHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Color(0xff101014),
         items: [
           BottomNavigationBarItem(
-              icon: Icon(
-                Icons.home_filled,
-              ),
-              label: "Home"),
+            icon: Icon(
+              Icons.home_filled,
+            ),
+            label: "Home",
+          ),
           BottomNavigationBarItem(
-              icon: Icon(
-                Icons.emoji_objects_outlined,
-              ),
-              label: "Feeds"),
+            icon: Icon(
+              Icons.emoji_objects_outlined,
+            ),
+            label: "Feeds",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.favorite_outline,
+            ),
+            label: "Likes",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.shopping_bag_outlined,
+            ),
+            label: "Carts",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.person_2_outlined,
+            ),
+            label: "Profiles",
+          ),
         ],
-        backgroundColor: Color(0xff101014),
+
       ),
     );
   }
