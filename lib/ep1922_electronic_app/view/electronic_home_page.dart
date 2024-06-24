2b37dc638ca263e6b633b8354d76680527739e32
JAICHANGPARK <aristojeff@gmail.com>
2024-06-24 20:05:07 +0900

    Update electronic_home_page.dart

--- lib/ep1922_electronic_app/view/electronic_home_page.dart
+++ lib/ep1922_electronic_app/view/electronic_home_page.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 
 class ElectronicHomePage extends StatefulWidget {
   const ElectronicHomePage({super.key});
@@ -14,27 +16,59 @@ class _ElectronicHomePageState extends State<ElectronicHomePage> {
       body: SafeArea(
         child: Column(
           children: [
-            Row(
-              children: [
-                Expanded(
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: Colors.white.withOpacity(.1),
-                    ),
-                    child: Row(
-                      children: [
-                        CircleAvatar(),
-                        Expanded(child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-
-                          ],
-                        ))
-                      ],
+            Padding(
+              padding: const EdgeInsets.fromLTRB(20.0, 32, 20, 20),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      padding: EdgeInsets.all(8),
+                      decoration: BoxDecoration(
+                        color: Color.fromRGBO(38, 39, 44, 1),
+                        borderRadius: BorderRadius.circular(32),
+                      ),
+                      child: Row(
+                        children: [
+                          CircleAvatar(
+                            radius: 24,
+                            child: Icon(
+                              CupertinoIcons.search,
+                            ),
+                            foregroundColor: Colors.white,
+                          ),
+                          Gap(12),
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  "Search on Electis",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                Text(
+                                  "Electronics Shoes Anything",
+                                  style: TextStyle(
+                                    color: Color.fromRGBO(29, 30, 34, 1),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
                     ),
                   ),
-                )
-              ],
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(
+                      Icons.menu,
+                    ),
+                    color: Colors.white,
+                  ),
+                ],
+              ),
             )
           ],
         ),
