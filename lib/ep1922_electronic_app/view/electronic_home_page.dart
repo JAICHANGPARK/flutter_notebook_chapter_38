6cdeae2308ceef3496e7e16ea0b2b41fb3bf6d14
JAICHANGPARK <aristojeff@gmail.com>
2024-06-26 20:30:58 +0900

    Update electronic_home_page.dart

--- lib/ep1922_electronic_app/view/electronic_home_page.dart
+++ lib/ep1922_electronic_app/view/electronic_home_page.dart
@@ -87,11 +87,11 @@ class _ElectronicHomePageState extends State<ElectronicHomePage> {
                 ],
               ),
             ),
-            Gap(12),
+            const Gap(12),
             Padding(
               padding: const EdgeInsets.only(bottom: 24),
               child: Container(
-                margin: EdgeInsets.only(left: 20),
+                margin: const EdgeInsets.only(left: 20),
                 height: 42,
                 child: ListView.builder(
                   scrollDirection: Axis.horizontal,
@@ -105,17 +105,17 @@ class _ElectronicHomePageState extends State<ElectronicHomePage> {
                       },
                       child: Container(
                         decoration: BoxDecoration(
-                          color: selectedTab == index ? Color(0xff4E53EE) : Colors.white.withOpacity(.1),
+                          color: selectedTab == index ? const Color(0xff4E53EE) : Colors.white.withOpacity(.1),
                           borderRadius: BorderRadius.circular(32),
                         ),
-                        margin: EdgeInsets.only(right: 12),
-                        padding: EdgeInsets.symmetric(
+                        margin: const EdgeInsets.only(right: 12),
+                        padding: const EdgeInsets.symmetric(
                           horizontal: 20,
                         ),
                         child: Center(
                             child: Text(
                           "${tabs[index]}",
-                          style: TextStyle(
+                          style: const TextStyle(
                             color: Colors.white,
                           ),
                         )),
@@ -133,44 +133,45 @@ class _ElectronicHomePageState extends State<ElectronicHomePage> {
                     SizedBox(
                       height: 200,
                       child: Container(
-                        margin: EdgeInsets.only(left: 20),
+                        margin: const EdgeInsets.only(left: 20),
                         child: PageView(
                           padEnds: false,
                           controller: pageController,
                           children: [
                             Container(
                               decoration: BoxDecoration(
-                                color: Color.fromRGBO(213, 196, 251, 1),
+                                color: const Color.fromRGBO(213, 196, 251, 1),
                                 borderRadius: BorderRadius.circular(12),
                               ),
-                              padding: EdgeInsets.all(20),
+                              padding: const EdgeInsets.all(20),
+                              margin: const EdgeInsets.only(right: 12),
                               child: Column(
                                 crossAxisAlignment: CrossAxisAlignment.start,
                                 children: [
-                                  Text(
+                                  const Text(
                                     "Try Bold",
                                     style: TextStyle(
                                       fontWeight: FontWeight.bold,
                                       fontSize: 24,
                                     ),
                                   ),
-                                  Text(
+                                  const Text(
                                     "Experience",
                                     style: TextStyle(
                                       fontWeight: FontWeight.bold,
                                       fontSize: 24,
                                     ),
                                   ),
-                                  Gap(8),
-                                  Text("S think diffrents"),
-                                  Gap(24),
+                                  const Gap(8),
+                                  const Text("S think diffrents"),
+                                  const Gap(24),
                                   Container(
-                                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 8),
+                                    padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 8),
                                     decoration: BoxDecoration(
                                       color: Colors.black,
                                       borderRadius: BorderRadius.circular(32),
                                     ),
-                                    child: Text(
+                                    child: const Text(
                                       "Discount 40%",
                                       style: TextStyle(
                                         color: Colors.white,
@@ -179,25 +180,24 @@ class _ElectronicHomePageState extends State<ElectronicHomePage> {
                                   )
                                 ],
                               ),
-                              margin: EdgeInsets.only(right: 12),
                             ),
                             Container(
-                              decoration: BoxDecoration(
+                              decoration: const BoxDecoration(
                                 color: Colors.red,
                               ),
-                              margin: EdgeInsets.only(right: 12),
+                              margin: const EdgeInsets.only(right: 12),
                             ),
                             Container(
-                              decoration: BoxDecoration(
+                              decoration: const BoxDecoration(
                                 color: Colors.orange,
                               ),
-                              margin: EdgeInsets.only(right: 12),
+                              margin: const EdgeInsets.only(right: 12),
                             )
                           ],
                         ),
                       ),
                     ),
-                    Gap(24),
+                    const Gap(24),
                     Center(
                       child: SizedBox(
                         height: 16,
@@ -214,13 +214,13 @@ class _ElectronicHomePageState extends State<ElectronicHomePage> {
                         ),
                       ),
                     ),
-                    Gap(12),
+                    const Gap(12),
                     Padding(
                       padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
                       child: Row(
                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
-                          Text(
+                          const Text(
                             "Electis Choice",
                             style: TextStyle(
                               fontSize: 20,
@@ -233,7 +233,7 @@ class _ElectronicHomePageState extends State<ElectronicHomePage> {
                               foregroundColor: Colors.grey,
                             ),
                             onPressed: () {},
-                            child: Text(
+                            child: const Text(
                               "See all",
                             ),
                           ),
@@ -241,9 +241,9 @@ class _ElectronicHomePageState extends State<ElectronicHomePage> {
                       ),
                     ),
                     Container(
-                      margin: EdgeInsets.only(left: 20),
+                      margin: const EdgeInsets.only(left: 20),
                       height: 240,
-                      decoration: BoxDecoration(
+                      decoration: const BoxDecoration(
                         color: Colors.blue,
                       ),
                       child: ListView.builder(
@@ -251,7 +251,7 @@ class _ElectronicHomePageState extends State<ElectronicHomePage> {
                         itemCount: 10,
                         itemBuilder: (context, index) {
                           return Container(
-                            margin: EdgeInsets.only(right: 12),
+                            margin: const EdgeInsets.only(right: 12),
                             color: Colors.grey,
                             width: 160,
                             child: Column(
@@ -264,7 +264,7 @@ class _ElectronicHomePageState extends State<ElectronicHomePage> {
                                     ),
                                     child: Stack(
                                       children: [
-                                        Positioned(child: Container(child: Row(
+                                        Positioned(child: Container(child: const Row(
                                           children: [
                                           ],
                                         ),))
@@ -272,12 +272,12 @@ class _ElectronicHomePageState extends State<ElectronicHomePage> {
                                     ),
                                   ),
                                 ),
-                                Gap(8),
-                                Text(
+                                const Gap(8),
+                                const Text(
                                   "Alpha 9 Mark 3 Body Only",
                                 ),
-                                Gap(4),
-                                Text("RP 24.500.000"),
+                                const Gap(4),
+                                const Text("RP 24.500.000"),
                               ],
                             ),
                           );
