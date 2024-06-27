3d8217f258d7a875410a8b0bebc202687b4ad3f5
JAICHANGPARK <aristojeff@gmail.com>
2024-06-27 20:23:13 +0900

    Update electronic_app_shell.dart

--- lib/ep1922_electronic_app/view/electronic_app_shell.dart
+++ lib/ep1922_electronic_app/view/electronic_app_shell.dart
@@ -9,9 +9,51 @@ class ElectronicAppShell extends StatefulWidget {
 }
 
 class _ElectronicAppShellState extends State<ElectronicAppShell> {
-  int selectedTab = 0;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      bottomNavigationBar: SizedBox(
+        height: 72,
+        child: BottomNavigationBar(
+          backgroundColor: const Color.fromRGBO(29, 30, 34, 1),
+          selectedItemColor: Colors.white,
+          unselectedItemColor: Colors.grey,
+          type: BottomNavigationBarType.fixed,
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.home_filled,
+              ),
+              label: "Home",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.emoji_objects_outlined,
+              ),
+              label: "Feeds",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.favorite_outline,
+              ),
+              label: "Likes",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.shopping_bag_outlined,
+              ),
+              label: "Carts",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.person_2_outlined,
+              ),
+              label: "Profiles",
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
