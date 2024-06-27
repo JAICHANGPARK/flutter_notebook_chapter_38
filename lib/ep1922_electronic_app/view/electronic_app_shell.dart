71e246230be3be2c6c2be5f1ff7f8577eb96a426
JAICHANGPARK <aristojeff@gmail.com>
2024-06-27 20:22:28 +0900

    Create electronic_app_shell.dart

---
+++ lib/ep1922_electronic_app/view/electronic_app_shell.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+
+class ElectronicAppShell extends StatefulWidget {
+  const ElectronicAppShell({super.key});
+
+  @override
+  State<ElectronicAppShell> createState() => _ElectronicAppShellState();
+}
+
+class _ElectronicAppShellState extends State<ElectronicAppShell> {
+  int selectedTab = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
