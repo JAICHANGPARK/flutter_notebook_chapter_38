c7e5688e1e914f785ec17fc4142b59c25a99ee1a
JAICHANGPARK <aristojeff@gmail.com>
2024-06-27 20:27:40 +0900

    Create electronic_home_detail_page.dart

---
+++ lib/ep1922_electronic_app/view/electronic_home_detail_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class ElectronicHomeDetailPage extends StatefulWidget {
+  const ElectronicHomeDetailPage({super.key});
+
+  @override
+  State<ElectronicHomeDetailPage> createState() => _ElectronicHomeDetailPageState();
+}
+
+class _ElectronicHomeDetailPageState extends State<ElectronicHomeDetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
