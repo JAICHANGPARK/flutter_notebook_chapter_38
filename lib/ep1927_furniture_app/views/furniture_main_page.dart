59e327bba39422ec28811a4997c2ff76dcc9d048
JAICHANGPARK <aristojeff@gmail.com>
2024-06-29 10:12:08 +0900

    Update furniture_main_page.dart

--- lib/ep1927_furniture_app/views/furniture_main_page.dart
+++ lib/ep1927_furniture_app/views/furniture_main_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 import '../model/furniture_tab.dart';
@@ -87,6 +88,12 @@ class _FurnitureMainPageState extends State<FurnitureMainPage> {
                     ),
                   )
                 ],
+              ),
+            ),
+            Gap(16),
+            Expanded(
+              child: Container(
+                color: Colors.white,
               ),
             )
           ],
