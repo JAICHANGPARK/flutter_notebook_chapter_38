3906416cb021d0cdc3c3f5711e053029e38f89e3
JAICHANGPARK <aristojeff@gmail.com>
2024-06-29 10:09:15 +0900

    Create furniture_tab.dart

---
+++ lib/ep1927_furniture_app/model/furniture_tab.dart
@@ -0,0 +1,28 @@
+class FurnitureTab {
+  String? tabTitle;
+  int? count;
+
+  FurnitureTab({
+    this.tabTitle,
+    this.count,
+  });
+}
+
+final furnitureTabs = [
+  FurnitureTab(
+    tabTitle: "ALL CATEGORIES",
+    count: 341,
+  ),
+  FurnitureTab(
+    tabTitle: "HOME",
+    count: 94,
+  ),
+  FurnitureTab(
+    tabTitle: "OFFICE",
+    count: 59,
+  ),
+  FurnitureTab(
+    tabTitle: "ALL CATEGORIES",
+    count: 341,
+  )
+];
