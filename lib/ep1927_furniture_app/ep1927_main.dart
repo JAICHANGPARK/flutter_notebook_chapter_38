587197319107a8f84af99885cda8429491d56428
JAICHANGPARK <aristojeff@gmail.com>
2024-06-29 09:57:32 +0900

    Update ep1927_main.dart

---
+++ lib/ep1927_furniture_app/ep1927_main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  
+}
+
+class FurnitureApp extends StatelessWidget {
+  const FurnitureApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
