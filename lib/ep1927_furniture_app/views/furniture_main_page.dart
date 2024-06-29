9fdc4f333bbda2f22a9cd26effba4fb0fc61e39b
JAICHANGPARK <aristojeff@gmail.com>
2024-06-29 10:03:36 +0900

    Update furniture_main_page.dart

--- lib/ep1927_furniture_app/views/furniture_main_page.dart
+++ lib/ep1927_furniture_app/views/furniture_main_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class FurnitureMainPage extends StatefulWidget {
   const FurnitureMainPage({super.key});
 
@@ -11,6 +10,48 @@ class FurnitureMainPage extends StatefulWidget {
 class _FurnitureMainPageState extends State<FurnitureMainPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Furniture",
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.search,
+            ),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.menu,
+            ),
+          ),
+        ],
+      ),
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [],
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.home_filled,
+            ),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.shopping_bag_outlined,
+            ),
+            label: "Cart",
+          )
+        ],
+      ),
+    );
   }
 }
