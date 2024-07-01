8ae30c65a3f9046acb169b2b07b74b8d2b34763a
JAICHANGPARK <aristojeff@gmail.com>
2024-07-01 18:25:05 +0900

    Update furniture_cart_page.dart

---
+++ lib/ep1927_furniture_app/views/furniture_cart_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class FurnitureCartPage extends StatefulWidget {
+  const FurnitureCartPage({super.key});
+
+  @override
+  State<FurnitureCartPage> createState() => _FurnitureCartPageState();
+}
+
+class _FurnitureCartPageState extends State<FurnitureCartPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.edit_square,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
