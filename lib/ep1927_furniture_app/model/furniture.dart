a7b7a59a62dfc34724b6db313eb563293dfc534a
JAICHANGPARK <aristojeff@gmail.com>
2024-07-02 18:42:52 +0900

    Update furniture.dart

--- lib/ep1927_furniture_app/model/furniture.dart
+++ lib/ep1927_furniture_app/model/furniture.dart
@@ -1,7 +1,28 @@
 class Furniture {
-
   String? img;
   String? title;
   double? price;
   int? count;
+
+  Furniture({
+    this.img,
+    this.title,
+    this.price,
+    this.count,
+  });
 }
+
+List<Furniture> furnitures = [
+  Furniture(
+    img: "https://cdn.pixabay.com/photo/2014/12/21/23/42/office-chair-575881_1280.png",
+    title: "STANCE DINNING",
+    price: 560.00,
+    count: 1,
+  ),
+  Furniture(
+    img: "https://cdn.pixabay.com/photo/2014/12/21/23/42/office-chair-575881_1280.png",
+    title: "COMPY CUSSION",
+    price: 560.00,
+    count: 1,
+  )
+];
