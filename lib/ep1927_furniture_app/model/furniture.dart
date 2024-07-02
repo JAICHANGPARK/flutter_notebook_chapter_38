74e33a61749fcdd2440c2830f5b3f60fe6ff5bbe
JAICHANGPARK <aristojeff@gmail.com>
2024-07-02 18:48:06 +0900

    Update furniture.dart

--- lib/ep1927_furniture_app/model/furniture.dart
+++ lib/ep1927_furniture_app/model/furniture.dart
@@ -24,5 +24,23 @@ List<Furniture> furnitures = [
     title: "COMPY CUSSION",
     price: 900.00,
     count: 1,
-  )
+  ),
+  Furniture(
+    img: "https://cdn.pixabay.com/photo/2014/12/21/23/49/couch-576126_1280.png",
+    title: "COMPY CUSSION",
+    price: 900.00,
+    count: 1,
+  ),
+  Furniture(
+    img: "https://cdn.pixabay.com/photo/2014/12/21/23/42/office-chair-575881_1280.png",
+    title: "STANCE DINNING",
+    price: 560.00,
+    count: 1,
+  ),
+  Furniture(
+    img: "https://cdn.pixabay.com/photo/2014/12/21/23/49/couch-576126_1280.png",
+    title: "COMPY CUSSION",
+    price: 900.00,
+    count: 1,
+  ),
 ];
