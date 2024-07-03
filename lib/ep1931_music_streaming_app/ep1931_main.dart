326272f58fe1923692451cc871643e72197452f0
JAICHANGPARK <aristojeff@gmail.com>
2024-07-03 18:34:54 +0900

    Update ep1931_main.dart

--- lib/ep1931_music_streaming_app/ep1931_main.dart
+++ lib/ep1931_music_streaming_app/ep1931_main.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 
 
+void main() {
+  
+}
 
 class MusicStreamApp extends StatelessWidget {
   const MusicStreamApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp();
   }
 }
