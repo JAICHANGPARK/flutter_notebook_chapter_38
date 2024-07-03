5c70751a5d463e04e6c662b654ce54a85a0bdcba
JAICHANGPARK <aristojeff@gmail.com>
2024-07-03 18:39:28 +0900

    Create music_stream_home_page.dart

---
+++ lib/ep1931_music_streaming_app/views/music_stream_home_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class MusicStreamHomePage extends StatefulWidget {
+  const MusicStreamHomePage({super.key});
+
+  @override
+  State<MusicStreamHomePage> createState() => _MusicStreamHomePageState();
+}
+
+class _MusicStreamHomePageState extends State<MusicStreamHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
