e6d4a0e8a1b0b6d668944dc0ae6a1bd1fd22e53f
JAICHANGPARK <aristojeff@gmail.com>
2024-07-04 18:41:39 +0900

    Update music_stream_library_widget.dart

--- lib/ep1931_music_streaming_app/views/widgets/music_stream_library_widget.dart
+++ lib/ep1931_music_streaming_app/views/widgets/music_stream_library_widget.dart
@@ -1,3 +1,16 @@
 import 'package:flutter/material.dart';
 
 
+class MusicStreamLibraryWidget extends StatefulWidget {
+  const MusicStreamLibraryWidget({super.key});
+
+  @override
+  State<MusicStreamLibraryWidget> createState() => _MusicStreamLibraryWidgetState();
+}
+
+class _MusicStreamLibraryWidgetState extends State<MusicStreamLibraryWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
