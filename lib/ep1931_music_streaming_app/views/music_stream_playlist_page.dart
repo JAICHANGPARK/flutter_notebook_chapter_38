51717a81a28f6739f32ca1a58a0b79a7c22e307f
JAICHANGPARK <aristojeff@gmail.com>
2024-07-04 18:43:54 +0900

    Update music_stream_playlist_page.dart

---
+++ lib/ep1931_music_streaming_app/views/music_stream_playlist_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class MusicStreamPlaylistPage extends StatefulWidget {
+  const MusicStreamPlaylistPage({super.key});
+
+  @override
+  State<MusicStreamPlaylistPage> createState() => _MusicStreamPlaylistPageState();
+}
+
+class _MusicStreamPlaylistPageState extends State<MusicStreamPlaylistPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
