4996155d7a3aa3762603907c22f9f69ee149505d
JAICHANGPARK <aristojeff@gmail.com>
2024-07-04 18:41:55 +0900

    Update music_stream_library_widget.dart

--- lib/ep1931_music_streaming_app/views/widgets/music_stream_library_widget.dart
+++ lib/ep1931_music_streaming_app/views/widgets/music_stream_library_widget.dart
@@ -11,6 +11,105 @@ class MusicStreamLibraryWidget extends StatefulWidget {
 class _MusicStreamLibraryWidgetState extends State<MusicStreamLibraryWidget> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Gap(24),
+          Column(
+            children: List.generate(
+              5,
+                  (index) {
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      radius: 28,
+                    ),
+                    title: Text("Playlists"),
+                    trailing: IconButton(
+                      onPressed: () {},
+                      icon: Icon(Icons.keyboard_arrow_right),
+                    ),
+                    titleTextStyle: TextStyle(
+                      fontSize: 20,
+                      color: Colors.black,
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+          Gap(12),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "Recently Added",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                  ),
+                ),
+                TextButton(
+                  onPressed: () {},
+                  child: Text(
+                    "See all",
+                  ),
+                )
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 16,
+            ),
+            child: Column(
+              children: List.generate(10, (index) {
+                return Padding(
+                  padding: const EdgeInsets.only(bottom: 12),
+                  child: Row(
+                    children: [
+                      Container(
+                        height: 72,
+                        width: 72,
+                        decoration: BoxDecoration(
+                          color: Colors.red,
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                      ),
+                      Gap(32),
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Stay",
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Text("The Kid, ... ... ...")
+                          ],
+                        ),
+                      ),
+                      IconButton(
+                        onPressed: () {},
+                        icon: Icon(
+                          Icons.more_vert,
+                        ),
+                        color: Colors.grey,
+                      ),
+                    ],
+                  ),
+                );
+              }).toList(),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
