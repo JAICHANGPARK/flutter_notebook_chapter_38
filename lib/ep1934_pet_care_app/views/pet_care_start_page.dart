a970a87811ca939b4e8526ef4a15c877fde45e06
JAICHANGPARK <aristojeff@gmail.com>
2024-07-06 08:18:07 +0900

    Update pet_care_start_page.dart

--- lib/ep1934_pet_care_app/views/pet_care_start_page.dart
+++ lib/ep1934_pet_care_app/views/pet_care_start_page.dart
@@ -1,13 +1,30 @@
 import 'package:flutter/material.dart';
 
-
 class PetCareStartPage extends StatelessWidget {
   const PetCareStartPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      body: Stack(
+        children: [
+          Positioned.fill(
+            child: Image.network(
+              "https://cdn.pixabay.com/photo/2023/05/23/15/26/bengal-cat-8012976_1280.jpg",
+              fit: BoxFit.cover,
+            ),
+          ),
+          Positioned(
+            top: 120,
+            bottom: 42,
+            left: 0,
+            right: 0,
+            child: Column(
+              children: [],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
