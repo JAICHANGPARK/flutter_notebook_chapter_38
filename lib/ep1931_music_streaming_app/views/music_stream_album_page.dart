58c6df98fcabf87ec77a5548a18f156baec9a350
JAICHANGPARK <aristojeff@gmail.com>
2024-07-06 07:51:46 +0900

    Update music_stream_album_page.dart

--- lib/ep1931_music_streaming_app/views/music_stream_album_page.dart
+++ lib/ep1931_music_streaming_app/views/music_stream_album_page.dart
@@ -19,124 +19,178 @@ class _MusicStreamAlbumPageState extends State<MusicStreamAlbumPage> {
         ),
         centerTitle: true,
       ),
-      body: Column(
-        children: [
-          Container(
-            margin: const EdgeInsets.all(16),
-            height: 440,
-            child: Stack(
-              children: [
-                // Positioned(
-                //   left: 16,
-                //   right: 16,
-                //   top: 0,
-                //   bottom: 62,
-                //   child: Container(
-                //     decoration: BoxDecoration(
-                //       color: Colors.purple[50],
-                //       borderRadius: BorderRadius.circular(24),
-                //     ),
-                //   ),
-                // ),
-                Positioned(
-                  left: 0,
-                  right: 0,
-                  top: 8,
-                  bottom: 100,
-                  child: GestureDetector(
-                    onTap: () {},
-                    child: Container(
-                      decoration: BoxDecoration(
-                        color: Colors.purple[200],
-                        borderRadius: BorderRadius.circular(24),
-                      ),
-                      child: const Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text(
-                            "Best Dance",
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 32,
-                              color: Colors.white,
-                            ),
-                          ),
-                          Gap(12),
-                          Text(
-                            "Songs 2024",
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 32,
-                              color: Colors.white,
-                            ),
-                          )
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-                Positioned(
-                  left: 0,
-                  bottom: 0,
-                  right: 0,
-                  child: Row(
-                    children: [
-                      Expanded(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              margin: const EdgeInsets.all(16),
+              height: 440,
+              child: Stack(
+                children: [
+                  // Positioned(
+                  //   left: 16,
+                  //   right: 16,
+                  //   top: 0,
+                  //   bottom: 62,
+                  //   child: Container(
+                  //     decoration: BoxDecoration(
+                  //       color: Colors.purple[50],
+                  //       borderRadius: BorderRadius.circular(24),
+                  //     ),
+                  //   ),
+                  // ),
+                  Positioned(
+                    left: 0,
+                    right: 0,
+                    top: 8,
+                    bottom: 100,
+                    child: GestureDetector(
+                      onTap: () {},
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: Colors.purple[200],
+                          borderRadius: BorderRadius.circular(24),
+                        ),
+                        child: const Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
                           children: [
                             Text(
-                              "Dance",
+                              "Best Dance",
                               style: TextStyle(
                                 fontWeight: FontWeight.bold,
-                                fontSize: 24,
+                                fontSize: 32,
+                                color: Colors.white,
                               ),
                             ),
-                            Gap(5),
+                            Gap(12),
                             Text(
-                              "134 Songs",
+                              "Songs 2024",
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 32,
+                                color: Colors.white,
+                              ),
                             )
                           ],
                         ),
                       ),
-                      CircleAvatar(
-                        backgroundColor: Colors.blue[50]!,
-                        foregroundColor: Colors.blue,
-                        child: Icon(Icons.shuffle),
-                        radius: 28,
-                      )
-                    ],
+                    ),
                   ),
-                ),
-                Positioned(
-                  right: 42,
-                  bottom: 50,
-                  child: Container(
-                    height: 80,
-                    width: 80,
-                    decoration: const BoxDecoration(shape: BoxShape.circle, color: Colors.white),
-                    padding: const EdgeInsets.all(8),
-                    child: Container(
-                      decoration: const ShapeDecoration(
-                        shape: PolygonBorder(sides: 8),
-                        color: Colors.black,
-                      ),
-                      child: IconButton(
-                        icon: const Icon(
-                          Icons.play_arrow,
+                  Positioned(
+                    left: 0,
+                    bottom: 0,
+                    right: 0,
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "Dance",
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 24,
+                                ),
+                              ),
+                              Gap(5),
+                              Text(
+                                "134 Songs",
+                              )
+                            ],
+                          ),
                         ),
-                        color: Colors.white,
-                        onPressed: () {},
-                        iconSize: 32,
+                        CircleAvatar(
+                          backgroundColor: Colors.blue[50]!,
+                          foregroundColor: Colors.blue,
+                          child: Icon(Icons.shuffle),
+                          radius: 28,
+                        )
+                      ],
+                    ),
+                  ),
+                  Positioned(
+                    right: 42,
+                    bottom: 50,
+                    child: Container(
+                      height: 80,
+                      width: 80,
+                      decoration: const BoxDecoration(shape: BoxShape.circle, color: Colors.white),
+                      padding: const EdgeInsets.all(8),
+                      child: Container(
+                        decoration: const ShapeDecoration(
+                          shape: PolygonBorder(sides: 8),
+                          color: Colors.black,
+                        ),
+                        child: IconButton(
+                          icon: const Icon(
+                            Icons.play_arrow,
+                          ),
+                          color: Colors.white,
+                          onPressed: () {},
+                          iconSize: 32,
+                        ),
                       ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-
-        ],
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 16,
+              ),
+              child: Column(
+                children: List.generate(10, (index) {
+                  return Padding(
+                    padding: const EdgeInsets.only(bottom: 12),
+                    child: Row(
+                      children: [
+                        Container(
+                          height: 72,
+                          width: 72,
+                          decoration: BoxDecoration(
+                            color: Colors.red,
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                        ),
+                        Gap(32),
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "Stay",
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Text("The Kid, ... ... ...")
+                            ],
+                          ),
+                        ),
+                        IconButton(
+                          onPressed: () {},
+                          icon: Icon(
+                            Icons.cloud_download,
+                          ),
+                          color: Colors.grey,
+                        ),
+                        IconButton(
+                          onPressed: () {},
+                          icon: Icon(
+                            Icons.apps,
+                          ),
+                          color: Colors.grey,
+                        ),
+                      ],
+                    ),
+                  );
+                }).toList(),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
