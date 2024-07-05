6b1c38ffa668b508eceef6d9771125f0e1eb2d8f
JAICHANGPARK <aristojeff@gmail.com>
2024-07-05 18:24:41 +0900

    Update music_stream_album_page.dart

--- lib/ep1931_music_streaming_app/views/music_stream_album_page.dart
+++ lib/ep1931_music_streaming_app/views/music_stream_album_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 
 class MusicStreamAlbumPage extends StatefulWidget {
   const MusicStreamAlbumPage({super.key});
@@ -16,10 +17,115 @@ class _MusicStreamAlbumPageState extends State<MusicStreamAlbumPage> {
           "Album",
         ),
         centerTitle: true,
-
       ),
       body: Column(
-        children: [],
+        children: [
+          Container(
+            height: 400,
+            child: Stack(
+              children: [
+                Positioned(
+                  left: 16,
+                  right: 16,
+                  top: 0,
+                  bottom: 62,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.purple[50],
+                      borderRadius: BorderRadius.circular(24),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  left: 0,
+                  right: 0,
+                  top: 16,
+                  bottom: 52,
+                  child: GestureDetector(
+                    onTap: () {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (context) => MusicStreamAlbumPage(),
+                        ),
+                      );
+                    },
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.purple[200],
+                        borderRadius: BorderRadius.circular(24),
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            "Best Dance",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16,
+                              color: Colors.white,
+                            ),
+                          ),
+                          Gap(12),
+                          Text(
+                            "Songs 2024",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16,
+                              color: Colors.white,
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                const Positioned(
+                  left: 0,
+                  bottom: 0,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Dance",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                        ),
+                      ),
+                      Gap(5),
+                      Text(
+                        "134 Songs",
+                      )
+                    ],
+                  ),
+                ),
+                Positioned(
+                  right: 24,
+                  bottom: 26,
+                  child: Container(
+                    height: 58,
+                    width: 58,
+                    decoration: const BoxDecoration(shape: BoxShape.circle, color: Colors.white),
+                    padding: EdgeInsets.all(8),
+                    child: Container(
+                      decoration: ShapeDecoration(
+                        shape: PolygonBorder(sides: 8),
+                        color: Colors.purple,
+                      ),
+                      child: IconButton(
+                        icon: Icon(
+                          Icons.play_arrow,
+                        ),
+                        color: Colors.white,
+                        onPressed: () {},
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
