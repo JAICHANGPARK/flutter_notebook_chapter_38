87e8c4575f4950436a71b57889a39005b183cdff
JAICHANGPARK <aristojeff@gmail.com>
2024-07-05 18:22:54 +0900

    Create music_stream_album_page.dart

---
+++ lib/ep1931_music_streaming_app/views/music_stream_album_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class MusicStreamAlbumPage extends StatefulWidget {
+  const MusicStreamAlbumPage({super.key});
+
+  @override
+  State<MusicStreamAlbumPage> createState() => _MusicStreamAlbumPageState();
+}
+
+class _MusicStreamAlbumPageState extends State<MusicStreamAlbumPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(
+        children: [],
+      ),
+    );
+  }
+}
