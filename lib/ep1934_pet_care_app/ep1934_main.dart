0ca022f09e1849f2485fb25dbd3293ec6b4bac22
JAICHANGPARK <aristojeff@gmail.com>
2024-07-06 08:12:41 +0900

    Update ep1934_main.dart

---
+++ lib/ep1934_pet_care_app/ep1934_main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(PetCareApp());
+}
+
+class PetCareApp extends StatelessWidget {
+  const PetCareApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
