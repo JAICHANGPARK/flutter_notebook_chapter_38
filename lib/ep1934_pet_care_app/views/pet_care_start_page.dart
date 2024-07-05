2c3e7415dd4ee59058fccd2cc0e089a6925a71dc
JAICHANGPARK <aristojeff@gmail.com>
2024-07-06 08:13:17 +0900

    Create pet_care_start_page.dart

---
+++ lib/ep1934_pet_care_app/views/pet_care_start_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class PetCareStartPage extends StatelessWidget {
+  const PetCareStartPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
