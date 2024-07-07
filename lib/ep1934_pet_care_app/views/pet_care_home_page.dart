ff1ba2a97a5a865a77b15c215ade451e83b60934
JAICHANGPARK <aristojeff@gmail.com>
2024-07-07 10:15:21 +0900

    Update pet_care_home_page.dart

---
+++ lib/ep1934_pet_care_app/views/pet_care_home_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PetCareHomePage extends StatefulWidget {
+  const PetCareHomePage({super.key});
+
+  @override
+  State<PetCareHomePage> createState() => _PetCareHomePageState();
+}
+
+class _PetCareHomePageState extends State<PetCareHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
