3ffda2b9e797969216b75fe6bcddbf61361315d8
JAICHANGPARK <aristojeff@gmail.com>
2024-07-09 22:33:09 +0900

    Create pet_profile_page.dart

---
+++ lib/ep1934_pet_care_app/views/pet_profile_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class PetProfilePage extends StatefulWidget {
+  const PetProfilePage({super.key});
+
+  @override
+  State<PetProfilePage> createState() => _PetProfilePageState();
+}
+
+class _PetProfilePageState extends State<PetProfilePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
