87296628512895beaa7a9fcb89b78dff84fb22df
JAICHANGPARK <aristojeff@gmail.com>
2024-07-09 22:43:41 +0900

    Update pet_profile_page.dart

--- lib/ep1934_pet_care_app/views/pet_profile_page.dart
+++ lib/ep1934_pet_care_app/views/pet_profile_page.dart
@@ -34,9 +34,10 @@ class _PetProfilePageState extends State<PetProfilePage> {
                     Text(
                       "Health Records",
                       style: TextStyle(
-                        fontSize: 24,
+                        fontSize: 22,
                       ),
                     ),
+                    Gap(8),
                     Container(
                       padding: EdgeInsets.only(left: 12),
                       margin: EdgeInsets.symmetric(vertical: 4),
@@ -139,6 +140,112 @@ class _PetProfilePageState extends State<PetProfilePage> {
                           Expanded(
                             child: Text(
                               "Allergies & Dietary",
+                              style: TextStyle(
+                                fontSize: 18,
+                              ),
+                            ),
+                          ),
+                          IconButton(
+                            onPressed: () {},
+                            icon: Icon(
+                              Icons.arrow_forward_ios,
+                            ),
+                          )
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            Gap(12),
+            Card(
+              child: Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Activity Log",
+                      style: TextStyle(
+                        fontSize: 22,
+                      ),
+                    ),
+                    Gap(8),
+
+                    Container(
+                      padding: EdgeInsets.only(left: 12),
+                      margin: EdgeInsets.symmetric(vertical: 4),
+                      decoration: BoxDecoration(
+                        color: Colors.grey[100],
+                        border: Border.all(
+                          color: Colors.grey,
+                        ),
+                        borderRadius: BorderRadius.circular(6),
+                      ),
+                      child: Row(
+                        children: [
+                          Expanded(
+                            child: Text(
+                              "Daily Exercise & Routine",
+                              style: TextStyle(
+                                fontSize: 18,
+                              ),
+                            ),
+                          ),
+                          IconButton(
+                            onPressed: () {},
+                            icon: Icon(
+                              Icons.arrow_forward_ios,
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                    Container(
+                      padding: EdgeInsets.only(left: 12),
+                      margin: EdgeInsets.symmetric(vertical: 4),
+                      decoration: BoxDecoration(
+                        color: Colors.grey[100],
+                        border: Border.all(
+                          color: Colors.grey,
+                        ),
+                        borderRadius: BorderRadius.circular(6),
+                      ),
+                      child: Row(
+                        children: [
+                          Expanded(
+                            child: Text(
+                              "Favourite Activity",
+                              style: TextStyle(
+                                fontSize: 18,
+                              ),
+                            ),
+                          ),
+                          IconButton(
+                            onPressed: () {},
+                            icon: Icon(
+                              Icons.arrow_forward_ios,
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                    Container(
+                      padding: EdgeInsets.only(left: 12),
+                      margin: EdgeInsets.symmetric(vertical: 4),
+                      decoration: BoxDecoration(
+                        color: Colors.grey[100],
+                        border: Border.all(
+                          color: Colors.grey,
+                        ),
+                        borderRadius: BorderRadius.circular(6),
+                      ),
+                      child: Row(
+                        children: [
+                          Expanded(
+                            child: Text(
+                              "Training Progress",
                               style: TextStyle(
                                 fontSize: 18,
                               ),
