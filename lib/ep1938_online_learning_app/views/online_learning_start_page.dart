fefc18d6e6794408da1a836b9b69fd3ec032bd20
JAICHANGPARK <aristojeff@gmail.com>
2024-07-10 20:31:01 +0900

    Update online_learning_start_page.dart

--- lib/ep1938_online_learning_app/views/online_learning_start_page.dart
+++ lib/ep1938_online_learning_app/views/online_learning_start_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:gap/gap.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class OnlineLearningStartPage extends StatelessWidget {
@@ -12,17 +13,21 @@ class OnlineLearningStartPage extends StatelessWidget {
       body: SafeArea(
         child: Column(
           children: [
+            Gap(42),
             Container(
-              height: 360,
+              height: 320,
               child: SvgPicture.network(
                 "https://assets-global.website-files.com/64200475697d5630983832ee/64f9ee0444229c0ff7e66a87_02.svg",
               ),
             ),
+            Gap(42),
             Text(
               "Pick an accessible\ncourse and embark on\nyour digital career\njourney with ease and\nconvenience.",
               style: GoogleFonts.sora(
                 fontWeight: FontWeight.bold,
+                fontSize: 24,
               ),
+              textAlign: TextAlign.center,
             )
           ],
         ),
