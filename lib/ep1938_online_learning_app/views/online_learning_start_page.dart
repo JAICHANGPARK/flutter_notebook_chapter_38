8bffbc42d205b38e4a65faa0af176a488b2110b7
JAICHANGPARK <aristojeff@gmail.com>
2024-07-10 20:27:36 +0900

    Update online_learning_start_page.dart

--- lib/ep1938_online_learning_app/views/online_learning_start_page.dart
+++ lib/ep1938_online_learning_app/views/online_learning_start_page.dart
@@ -7,6 +7,18 @@ class OnlineLearningStartPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Column(
+          children: [
+            Container(
+              height: 360,
+              child: Image.network("https://assets-global.website-files.com/64200475697d5630983832ee/64f9ee0444229c0ff7e66a87_02.svg",),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
