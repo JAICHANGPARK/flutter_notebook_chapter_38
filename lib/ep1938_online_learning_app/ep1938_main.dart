48f4fdef3238caf671cb4e3ba14863dbb958cbf5
JAICHANGPARK <aristojeff@gmail.com>
2024-07-10 20:19:27 +0900

    Create ep1938_main.dart

---
+++ lib/ep1938_online_learning_app/ep1938_main.dart
@@ -0,0 +1,12 @@
+
+
+import 'package:flutter/material.dart';
+
+class OnlineLearningApp extends StatelessWidget {
+  const OnlineLearningApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
