1c827d7c9b09717b9b2cca14306034d3e8e5af5e
JAICHANGPARK <aristojeff@gmail.com>
2024-07-11 20:08:15 +0900

    Update online_learning_home_page.dart

---
+++ lib/ep1938_online_learning_app/views/online_learning_home_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class OnlineLearningHomePage extends StatefulWidget {
+  const OnlineLearningHomePage({super.key});
+
+  @override
+  State<OnlineLearningHomePage> createState() => _OnlineLearningHomePageState();
+}
+
+class _OnlineLearningHomePageState extends State<OnlineLearningHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            
+          ],
+        ),
+      ),
+    );
+  }
+}
