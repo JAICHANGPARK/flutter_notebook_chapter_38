c580aedb4e45a0b5400fdb232726ff8018a9c9f9
JAICHANGPARK <aristojeff@gmail.com>
2024-07-13 09:11:26 +0900

    Update fitness_tracker_overview_widget.dart

---
+++ lib/ep1941_fitness_tracker_app/views/fitness_tracker_overview_widget.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+
+class FitnessTrackerOverviewWidget extends StatefulWidget {
+  const FitnessTrackerOverviewWidget({super.key});
+
+  @override
+  State<FitnessTrackerOverviewWidget> createState() => _FitnessTrackerOverviewWidgetState();
+}
+
+class _FitnessTrackerOverviewWidgetState extends State<FitnessTrackerOverviewWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+
+      ],
+    );
+  }
+}
