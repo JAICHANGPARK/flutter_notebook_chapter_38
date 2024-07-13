8d7f3c2191c5a3ca3fed1ed6482e531e18fd4f8b
JAICHANGPARK <aristojeff@gmail.com>
2024-07-13 09:24:57 +0900

    Create fitness_tracker_activity_widget.dart

---
+++ lib/ep1941_fitness_tracker_app/views/fitness_tracker_activity_widget.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class FitnessTrackerActivityWidget extends StatefulWidget {
+  const FitnessTrackerActivityWidget({super.key});
+
+  @override
+  State<FitnessTrackerActivityWidget> createState() => _FitnessTrackerActivityWidgetState();
+}
+
+class _FitnessTrackerActivityWidgetState extends State<FitnessTrackerActivityWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
