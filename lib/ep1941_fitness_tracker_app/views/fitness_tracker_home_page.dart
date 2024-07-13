690dd77943fdf452e430c17a8a5c9431ebc1a104
JAICHANGPARK <aristojeff@gmail.com>
2024-07-13 09:09:06 +0900

    Create fitness_tracker_home_page.dart

---
+++ lib/ep1941_fitness_tracker_app/views/fitness_tracker_home_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class FitnessTrackerHomePage extends StatefulWidget {
+  const FitnessTrackerHomePage({super.key});
+
+  @override
+  State<FitnessTrackerHomePage> createState() => _FitnessTrackerHomePageState();
+}
+
+class _FitnessTrackerHomePageState extends State<FitnessTrackerHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
