37086b4cdcc2e1fa6f9141738d8508fcde6620f3
JAICHANGPARK <aristojeff@gmail.com>
2024-07-14 10:23:41 +0900

    Update ep1941_main.dart

--- lib/ep1941_fitness_tracker_app/ep1941_main.dart
+++ lib/ep1941_fitness_tracker_app/ep1941_main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_chapter_38/ep1941_fitness_tracker_app/views/fitness_tracker_activity_widget.dart';
 import 'package:flutter_notebook_chapter_38/ep1941_fitness_tracker_app/views/fitness_tracker_home_page.dart';
 import 'package:flutter_notebook_chapter_38/ep1941_fitness_tracker_app/views/fitness_tracker_overview_widget.dart';
 import 'package:go_router/go_router.dart';
@@ -29,12 +30,21 @@ class FitnessTrackerApp extends StatelessWidget {
             },
             branches: [
               StatefulShellBranch(
-                initialLocation: "/overview",
                 routes: [
                   GoRoute(
                     path: "/overview",
                     builder: (context, state) {
                       return FitnessTrackerOverviewWidget();
+                    },
+                  ),
+                ],
+              ),
+              StatefulShellBranch(
+                routes: [
+                  GoRoute(
+                    path: "/activity",
+                    builder: (context, state) {
+                      return FitnessTrackerActivityWidget();
                     },
                   ),
                 ],
