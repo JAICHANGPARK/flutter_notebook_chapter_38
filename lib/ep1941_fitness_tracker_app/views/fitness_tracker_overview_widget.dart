9598f4642c551a59b79414a281e61999bfda7726
JAICHANGPARK <aristojeff@gmail.com>
2024-07-14 10:37:09 +0900

    Update fitness_tracker_overview_widget.dart

--- lib/ep1941_fitness_tracker_app/views/fitness_tracker_overview_widget.dart
+++ lib/ep1941_fitness_tracker_app/views/fitness_tracker_overview_widget.dart
@@ -16,131 +16,136 @@ class _FitnessTrackerOverviewWidgetState extends State<FitnessTrackerOverviewWid
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Row(
-            children: [
-              Expanded(
-                child: Container(
-                  height: 120,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(8),
-                    border: Border.all(
-                      color: Colors.grey,
+          SizedBox(
+            height: 140,
+            child: Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8),
+                      border: Border.all(
+                        color: Colors.grey,
+                      ),
+                    ),
+                    padding: EdgeInsets.all(12),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          children: [
+                            Icon(
+                              Icons.directions_run,
+                              size: 14,
+                              color: Colors.lightGreenAccent,
+                            ),
+                            Gap(4),
+                            Text(
+                              "Steps",
+                            )
+                          ],
+                        ),
+                        Spacer(),
+                        Row(
+                          children: [
+                            Column(
+                              children: [
+                                Text.rich(
+                                  TextSpan(
+                                    children: [
+                                      TextSpan(
+                                        text: "6.4",
+                                        style: TextStyle(
+                                          fontSize: 24,
+                                        )
+                                      ),
+                                      TextSpan(
+                                        text: " km",
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                                Row(
+                                  children: [
+                                    Icon(
+                                      Icons.arrow_drop_up,
+                                      color: Colors.lightGreen,
+                                    ),
+                                    Text("3.8 km")
+                                  ],
+                                )
+                              ],
+                            ),
+
+                          ],
+                        )
+                      ],
                     ),
                   ),
-                  padding: EdgeInsets.all(12),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Row(
-                        children: [
-                          Icon(
-                            Icons.directions_run,
-                            size: 14,
-                            color: Colors.lightGreenAccent,
-                          ),
-                          Gap(4),
-                          Text(
-                            "Steps",
-                          )
-                        ],
-                      ),
-                      Spacer(),
-                      Row(
-                        children: [
-                          Column(
-                            children: [
-                              Text.rich(
-                                TextSpan(
-                                  children: [
-                                    TextSpan(
-                                      text: "6.4",
-                                    ),
-                                    TextSpan(
-                                      text: " km",
-                                    ),
-                                  ],
-                                ),
-                              ),
-                              Row(
-                                children: [
-                                  Icon(
-                                    Icons.arrow_drop_up,
-                                    color: Colors.lightGreen,
-                                  ),
-                                  Text("3.8 km")
-                                ],
-                              )
-                            ],
-                          ),
-
-                        ],
-                      )
-                    ],
-                  ),
                 ),
-              ),
-              Gap(8),
-              Expanded(
-                child: Container(
-                  height: 120,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(8),
-                    border: Border.all(
-                      color: Colors.grey,
+                Gap(8),
+                Expanded(
+                  child: Container(
+
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8),
+                      border: Border.all(
+                        color: Colors.grey,
+                      ),
+                    ),
+                    padding: EdgeInsets.all(12),
+                    child: Column(
+                      children: [
+                        Row(
+                          children: [
+                            Icon(
+                              Icons.directions_run,
+                              size: 14,
+                              color: Colors.lightGreenAccent,
+                            ),
+                            Gap(4),
+                            Text(
+                              "Steps",
+                            )
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text.rich(
+                                  TextSpan(
+                                    children: [
+                                      TextSpan(
+                                        text: "6.4",
+                                      ),
+                                      TextSpan(
+                                        text: " km",
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                                Row(
+                                  children: [
+                                    Icon(
+                                      Icons.arrow_drop_up,
+                                      color: Colors.lightGreen,
+                                    ),
+                                    Text("3.8 km")
+                                  ],
+                                )
+                              ],
+                            ),
+
+                          ],
+                        )
+                      ],
                     ),
                   ),
-                  padding: EdgeInsets.all(12),
-                  child: Column(
-                    children: [
-                      Row(
-                        children: [
-                          Icon(
-                            Icons.directions_run,
-                            size: 14,
-                            color: Colors.lightGreenAccent,
-                          ),
-                          Gap(4),
-                          Text(
-                            "Steps",
-                          )
-                        ],
-                      ),
-                      Row(
-                        children: [
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text.rich(
-                                TextSpan(
-                                  children: [
-                                    TextSpan(
-                                      text: "6.4",
-                                    ),
-                                    TextSpan(
-                                      text: " km",
-                                    ),
-                                  ],
-                                ),
-                              ),
-                              Row(
-                                children: [
-                                  Icon(
-                                    Icons.arrow_drop_up,
-                                    color: Colors.lightGreen,
-                                  ),
-                                  Text("3.8 km")
-                                ],
-                              )
-                            ],
-                          ),
-
-                        ],
-                      )
-                    ],
-                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           )
         ],
       ),
