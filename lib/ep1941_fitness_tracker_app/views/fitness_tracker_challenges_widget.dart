ff78e3be3d93aaf220e7f0d54c83fcde4bc05507
JAICHANGPARK <aristojeff@gmail.com>
2024-07-17 20:00:03 +0900

    Update fitness_tracker_challenges_widget.dart

--- lib/ep1941_fitness_tracker_app/views/fitness_tracker_challenges_widget.dart
+++ lib/ep1941_fitness_tracker_app/views/fitness_tracker_challenges_widget.dart
@@ -1,1 +1,18 @@
 import 'package:flutter/material.dart';
+
+class FitnessTrackerChallengesWidget extends StatefulWidget {
+  const FitnessTrackerChallengesWidget({super.key});
+
+  @override
+  State<FitnessTrackerChallengesWidget> createState() => _FitnessTrackerChallengesWidgetState();
+}
+
+class _FitnessTrackerChallengesWidgetState extends State<FitnessTrackerChallengesWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [],
+    );
+  }
+}
