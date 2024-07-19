482bb3d9f3552c547c371acf5b3ea695dc904ad2
JAICHANGPARK <aristojeff@gmail.com>
2024-07-19 19:15:27 +0900

    Update ep1947_main.dart

---
+++ lib/ep1947_architecture_app/ep1947_main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+
+void main() {
+
+}
+
+
+class ArchitectureApp extends StatelessWidget {
+  const ArchitectureApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      
+    );
+  }
+}
