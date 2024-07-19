e6349cfc85a7cfdc94234ab7d8ad84beacb5ef8e
JAICHANGPARK <aristojeff@gmail.com>
2024-07-19 19:16:05 +0900

    Update architecture_main_page.dart

---
+++ lib/ep1947_architecture_app/view/architecture_main_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+
+class ArchitectureMainPage extends StatefulWidget {
+  const ArchitectureMainPage({super.key});
+
+  @override
+  State<ArchitectureMainPage> createState() => _ArchitectureMainPageState();
+}
+
+class _ArchitectureMainPageState extends State<ArchitectureMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+    );
+  }
+}
