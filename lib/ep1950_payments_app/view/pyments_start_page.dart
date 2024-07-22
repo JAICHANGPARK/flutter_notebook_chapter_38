2d172b32386ff7850567a690b892ce5837c94a17
JAICHANGPARK <aristojeff@gmail.com>
2024-07-22 19:12:04 +0900

    Create pyments_start_page.dart

---
+++ lib/ep1950_payments_app/view/pyments_start_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+
+class PaymentsStartPage extends StatefulWidget {
+  const PaymentsStartPage({super.key});
+
+  @override
+  State<PaymentsStartPage> createState() => _PaymentsStartPageState();
+}
+
+class _PaymentsStartPageState extends State<PaymentsStartPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
