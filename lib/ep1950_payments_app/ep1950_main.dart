5f1e7aef71c3eb533748d852fd93faf65d2f0d88
JAICHANGPARK <aristojeff@gmail.com>
2024-07-22 19:10:52 +0900

    Create ep1950_main.dart

---
+++ lib/ep1950_payments_app/ep1950_main.dart
@@ -0,0 +1,13 @@
+
+void main() {
+
+}
+
+class PaymentsApp extends StatelessWidget {
+  const PaymentsApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
