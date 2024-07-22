245975e3afc8d9b10ce881aad9ffd8b320daf571
JAICHANGPARK <aristojeff@gmail.com>
2024-07-22 19:16:49 +0900

    Update pyments_start_page.dart

--- lib/ep1950_payments_app/view/pyments_start_page.dart
+++ lib/ep1950_payments_app/view/pyments_start_page.dart
@@ -11,40 +11,51 @@ class _PaymentsStartPageState extends State<PaymentsStartPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color.fromRGBO(88, 184, 255, 1),
       body: SafeArea(
-        child: Column(
-          children: [
-            Row(
-              children: [
-                Text("Pay"),
-                Icon(
-                  Icons.check_circle,
-                ),
-                Spacer(),
-                IconButton(
-                  onPressed: () {},
-                  icon: Icon(
-                    Icons.menu,
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Text(
+                    "Pay",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 24,
+                    ),
                   ),
-                ),
-              ],
-            ),
-            Text("Payment\nMagic"),
-            Text(
-              "The modern capital platform for\nGrowth on your terms",
-            ),
-            Container(
-              decoration: BoxDecoration(),
-              child: Text("Open Account"),
-            ),
-            Text("*No Credit Score Required Any Further"),
-            Spacer(),
-            Container(
-              decoration: BoxDecoration(
-                color: Colors.white,
+                  Icon(
+                    Icons.check_circle,
+                    size: 16,
+                  ),
+                  Spacer(),
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(
+                      Icons.menu,
+                    ),
+                  ),
+                ],
               ),
-            )
-          ],
+              Text("Payment\nMagic"),
+              Text(
+                "The modern capital platform for\nGrowth on your terms",
+              ),
+              Container(
+                decoration: BoxDecoration(),
+                child: Text("Open Account"),
+              ),
+              Text("*No Credit Score Required Any Further"),
+              Spacer(),
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
